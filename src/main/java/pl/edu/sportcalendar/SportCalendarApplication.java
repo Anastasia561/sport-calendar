bdd814a49de3f8eebc2c14e0a9ce6233541fd3a9
Anastasia561 <kurulenkoanastasia@gmail.com>
2026-03-22 12:56:00 +0100

    init: initial commit

---
+++ src/main/java/pl/edu/sportcalendar/SportCalendarApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.sportcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/sportcalendar/SportCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.sportcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
